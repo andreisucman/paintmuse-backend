0b53551b56849a3567ea4c448359b801bc5c0466
Andrei Sucman <sucmanandrei@gmail.com>
2022-11-30 20:27:21 +0300

    Test

--- cloud/functions/registerUser.js
+++ cloud/functions/registerUser.js
@@ -40,7 +40,7 @@ Parse.Cloud.define("register", async (req) => {
     }
 
     console.log("Reached here 42")
-    await user.signUp({ useMasterKey: true });
+    await user.signUp(null, { useMasterKey: true });
     
     return { code: 0, user };
 
