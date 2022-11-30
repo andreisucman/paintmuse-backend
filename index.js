c3c857b69f1d9ae5f37d5c1e075ce430b54e6a56
Andrei Sucman <sucmanandrei@gmail.com>
2022-11-30 22:48:22 +0300

    test

--- index.js
+++ index.js
@@ -169,8 +169,6 @@ app.post("/checkout_sessions", async (req, res) => {
         customer_email: req.body.email,
       });
 
-      const stripe = await getStripe();
-      await stripe.redirectToCheckout({ sessionId: session.id });
       res.status(200).json(session);
     } catch (err) {
       res.status(500).json({ statusCode: 500, message: err.message });
