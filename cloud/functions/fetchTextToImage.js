47ba7c8cb2f0ce16aabe478013ea3510d670b2c2
Andrei Sucman <sucmanandrei@gmail.com>
2022-12-06 16:19:54 +0300

    Test

--- cloud/functions/fetchTextToImage.js
+++ cloud/functions/fetchTextToImage.js
@@ -41,7 +41,7 @@ Parse.Cloud.define("fetchTextToImage", async (req) => {
   } else {
     if (fetchOnce) {
       let index;
-      const q = Parse.Query("TextToImage");
+      const q = new Parse.Query("TextToImage");
       q.descending("createdAt");
       const r = await q.first();
 
@@ -49,7 +49,7 @@ Parse.Cloud.define("fetchTextToImage", async (req) => {
         index = r.attributes.index;
       }
 
-      const newQ = Parse.Query("TextToImage");;
+      const newQ = new Parse.Query("TextToImage");;
       newQ.equalTo("index", index);
       newQ.select(fields);
 
